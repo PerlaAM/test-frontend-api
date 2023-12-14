d45a0b93ea2150c11b74d1e7010fee6ffd5c66b4
Perla Aguilar <paguilar@ibl.mx>
2023-12-14 09:01:49 -0600

    Authorization & getProductsList

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@
 const path = require("path");
 
 module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/authenticate",
+                destination: `${process.env.SERVER}/service/preprod/authenticate?idDistribuidor=83&codigoDispositivo=GPS83-TPV-99&password=V7p6r2v1W1l4`,
+            },
+            {
+                source: "/api/getProductsList",
+                destination: `${process.env.SERVER}/service/preprod/getProductList.do`,
+            },
+        ];
+    },
+    env: {
+        X_API_KEY: process.env.X_API_KEY,
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
     },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,57 @@
-export default function Home() {
-    return <section></section>;
-}
+"use client";
+import { useState, useEffect } from "react";
+import Loader from "./components/loader";
+
+const Home = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+
+        fetch("/api/authenticate", {
+            method: "POST",
+            headers: {
+                "X-API-Key": `${process.env.X_API_KEY}`,
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `Error en la solicitud: ${response.status}`
+                    );
+                }
+                return response.json();
+            })
+            .then((data) => {
+                sessionStorage.setItem("authTokenTest", data.token);
+
+                return fetch("/api/getProductsList", {
+                    headers: {
+                        "X-API-Key": `${process.env.X_API_KEY}`,
+                        Authorization: `Bearer ${data.token}`,
+                    },
+                });
+            })
+            .then((productsResponse) => {
+                if (!productsResponse.ok) {
+                    throw new Error(
+                        `Error en la solicitud de productos: ${productsResponse.status}`
+                    );
+                }
+                return productsResponse.json();
+            })
+            .then((productsData) => {
+                console.log(productsData);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    return <section className="h-full">{loading ? <Loader /> : null}</section>;
+};
+
+export default Home;
